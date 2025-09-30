2ea026e9f7ef0199ae642039d425dddffcc30f92
Colm Roach <croach01@uoguelph.ca>
2025-09-30 10:57:35 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -3,5 +3,19 @@
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
+while(true)
+  depth = input("Enter your desired depth: ")
+  if depth.lstrip('-').isdigit() and depth > 0:
+    print("good")
+    break
+  else:
+    print("Invalid Input")
+
+
+def fibonacci (val1, val2, depth, currentDepth)
+  if currentDepth = depth:
+    return 0;
+  else:
+    return 
 # Validate that the input is a positive integer.
 # Use a for loop to print the Fibonacci sequence up to that many terms.
